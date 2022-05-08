bebcb55e85fec26e7e5ddae75dcdd96666a9d204
Benw8888 <benwright8888@yahoo.com>
2022-05-08 19:32:35 -0400

    Code Runs Now
    Code runs, spectral method still too slow (because of bug?)

--- example/Burgers/src/Burgers_comparison.jl
+++ example/Burgers/src/Burgers_comparison.jl
@@ -7,8 +7,7 @@ x_resolution = 2^10  # number of data points the x axis is split into
 dsamples = round(Int,2^13/x_resolution)  # how many other samples we pick from the base 2^13 dataset
 data = Burgers.get_dataloader(Δsamples=dsamples)  # get data from Zongyi
 _ ,test_data = data  # split data into train and test data
-
-print(test_data)
+spectral_dt = 0.1
 
 FNO_learner = Burgers.train(input_data=data, epochs=1)
 FNO_model = FNO_learner.model
@@ -21,23 +20,33 @@ fno_loss = 0
 spectral_loss = 0
 counter = 0
 
+no_change_loss = 0
+
 for (x,y) in test_data
     mini_batch_size = size(x)[3] # 100, 100, 5
     for i in 1:mini_batch_size
         counter = counter + 1
+        print(counter)
         data_point = x[:,:,i:i]
+        no_change_loss += loss_fn(data_point[2:2,:,:],y[:,:,i:i])
 
-        #fno_res = FNO_model(data_point)
-        spectral_res = spectral_output(n=x_resolution, input_data=data_point[2,:,:], viscosity=0.1)
+        dfno_time = @elapsed fno_res = FNO_model(data_point)
+        fno_time += dfno_time
 
-        #fno_time += @elapsed FNO_model(data_point)
-        #spectral_time += @elapsed spectral_output(input_data=data_point[2,:], viscosity=0.1)
+        dspectral_time = @elapsed spectral_res = spectral_output(n=x_resolution, input_data=data_point[2,:,1], viscosity=0.1, dt=spectral_dt)
+        spectral_time += dspectral_time
 
-        #fno_loss += loss_fn(fno_res,y[:,:,i:i])
-        #spectral_loss += loss_fn(spectral_res,y[:,:,i:i])
+        fno_loss += loss_fn(fno_res,y[:,:,i:i])
+        spectral_loss += loss_fn(reshape(spectral_res,(1,x_resolution,1)),y[:,:,i:i])
     end
 end
 
-    #counter = counter + 1
-    #spectral_result = spectral_output(x,0.1)
-    #spectral_time = spectral_time + @elapsed spec
+    # to get loss and time, divide fno_loss or spectral_loss and fno_time or spectral_time by counter
+    # you need to divide by counter to get an average
+
+println("PRINTING LOSSES")
+println("FNO Average Loss: ",fno_loss/counter)
+println("Spectral Time Iteration Average Loss: ",spectral_loss/counter)
+
+println("FNO Average Time: ",fno_time/counter)
+println("Spectral Time Iteration Average Time: ",spectral_time/counter)
--- example/Burgers/src/Burgers_spectral.jl
+++ example/Burgers/src/Burgers_spectral.jl
@@ -3,7 +3,7 @@ using DiffEqDevTools
 using LinearAlgebra
 using Plots; gr()
 
-function spectral_output(;n, input_data, viscosity)
+function spectral_output(;n, input_data, viscosity, dt)
     # Uses spectral method to get an output for a Burgers PDE input
     # Viscosity is a constant in the Burgers equation. Should be 0.1
     # n is the resolution (# of data points) of the input
@@ -15,7 +15,7 @@ function spectral_output(;n, input_data, viscosity)
     T = ApproxFun.plan_transform(S, n)
     Ti = ApproxFun.plan_itransform(S, n)
 
-    û₀ = input_data
+    û₀ = T * input_data
     A = viscosity*D2
     tmp = similar(û₀)
     p = (D,D2,T,Ti,tmp,similar(tmp))
@@ -29,11 +29,11 @@ function spectral_output(;n, input_data, viscosity)
         @. dû = - u
     end
 
-    prob = SplitODEProblem(DiffEqArrayOperator(Diagonal(A)), burgers_nl, û₀, (0.0,5.0), p)
-    sol  = solve(prob, Rodas5(autodiff=false); reltol=1e-12,abstol=1e-12)
+    prob = SplitODEProblem(DiffEqArrayOperator(Diagonal(A)), burgers_nl, û₀, (0.0,1.0), p)
+    sol  = solve(prob, Rodas5(autodiff=false), dt=dt)
     test_sol = TestSolution(sol)
 
-    return sol
+    return Ti*sol(1)
 end
 
 
