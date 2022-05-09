aef4d81296b1655439cc3fa077fc60e745a985c3
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2022-05-09 01:13:58 -0400

    Delete example/FlowOverCircle directory

--- example/FlowOverCircle/test/runtests.jl
+++
@@ -1,9 +0,0 @@
-using FlowOverCircle
-using FluxTraining
-using Test
-
-@testset "FlowOverCircle" begin
-    learner = FlowOverCircle.train(epochs=5)
-    loss = learner.cbstate.metricsepoch[ValidationPhase()][:Loss].values[end]
-    @test loss < 0.1
-end
